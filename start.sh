4aaa30bd3d1c8c59c58839a4bea31fdcd3f646cf
ganjang-gyelanbab <165130067+ganjang-gyelanbab@users.noreply.github.com>
2025-06-08 20:53:41 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# models 폴더 만들기
 mkdir -p models
 
-# 구글 드라이브 파일 ID
 FILE_ID="1yJLDbWkv4gC2O8t8tCl13R-Ea6ZUvh9R"
+COOKIE_FILE="/tmp/cookie.txt"
 
-# 다운로드 URL 생성
-DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=${FILE_ID}"
+# 구글 드라이브 확인 토큰 받아오기 + 실제 다운로드
+wget --quiet --save-cookies $COOKIE_FILE --keep-session-cookies --no-check-certificate \
+     "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O- | \
+     sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > /tmp/confirm.txt
 
-# 모델 다운로드 (wget으로 다운로드, 확인 페이지 우회 안 할 경우 간단히)
-wget --no-check-certificate "${DOWNLOAD_URL}" -O models/model.tar.gz
+CONFIRM=$(< /tmp/confirm.txt)
 
-# 압축 해제
+wget --load-cookies $COOKIE_FILE "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" \
+     -O models/model.tar.gz
+
+rm -f $COOKIE_FILE /tmp/confirm.txt
+
 tar -xzf models/model.tar.gz -C models/
 
-# Rasa 서버 실행 (models 폴더를 모델 경로로 지정)
 rasa run --enable-api --port 5005 --model models
