fe02619e99da95b0ed71c481c54845288944dcac
ganjang-gyelanbab <165130067+ganjang-gyelanbab@users.noreply.github.com>
2025-06-08 20:28:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# models 폴더 없으면 생성
+# models 폴더 만들기
 mkdir -p models
 
-# 구글 드라이브 파일 ID (자신 파일 ID로 바꿔주세요)
+# 구글 드라이브 파일 ID
 FILE_ID="1yJLDbWkv4gC2O8t8tCl13R-Ea6ZUvh9R"
 
-# 구글 드라이브 직접 다운로드 URL 생성
+# 다운로드 URL 생성
 DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=${FILE_ID}"
 
-# wget으로 모델 파일 다운로드 (models/model.tar.gz에 저장)
+# 모델 다운로드 (wget으로 다운로드, 확인 페이지 우회 안 할 경우 간단히)
 wget --no-check-certificate "${DOWNLOAD_URL}" -O models/model.tar.gz
 
-# Rasa 서버 실행 (포트 5005, 모델 경로 지정)
-rasa run --enable-api --port 5005 --model models/model.tar.gz
+# 압축 해제
+tar -xzf models/model.tar.gz -C models/
+
+# Rasa 서버 실행 (models 폴더를 모델 경로로 지정)
+rasa run --enable-api --port 5005 --model models
